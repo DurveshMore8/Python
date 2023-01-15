df1d7a726e92f7a9d8fe8e01895fb633b65eda58
Durvesh <durveshmore2003@gmail.com>
2023-01-15 17:26:21 +0530

    Programs Completed

--- First/3.py
+++ First/3.py
@@ -1,1 +1,13 @@
 # Write a program to generate the Fibonacci series
+
+length = int(input('Enter length of Fibonacci Series:'))
+first = 0
+second = 1
+
+print('Fibonacci Series:',end=' ')
+print(first,second,end=' ')
+for i in range (2, length) :
+    sum = first + second
+    first = second
+    second = sum
+    print(sum, end=' ')
